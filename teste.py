7979b4f72ec0015b1399d1b2c10a780e0dac287c
Augusto Lopez Dantas <augustold42@gmail.com>
2015-07-06 16:46:51 -0300

    file read

---
+++ teste.py
@@ -0,0 +1,22 @@
+begin = None
+end = None
+path = []
+heuristic = []
+
+def readFile():
+    global begin, end
+    for line in open("./files/entrada.txt", 'r', encoding='utf-8'):
+        line = line.rstrip(").\n").split("(")
+        print(line)
+        if(line[0] == "início"):
+            begin = line[1]
+        elif(line[0] == "final"):
+            end = line[1]
+        elif(line[0] == 'caminho'):
+            path.append(tuple(line[1].split(',')))
+        elif(line[0] == 'h'):
+            heuristic.append(tuple(line[1].split(',')))
+
+readFile()
+print(begin, end, path, heuristic)
+
