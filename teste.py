9a302b09667cdf380ce5d35fee89f1c9bcd7e819
Augusto Lopez Dantas <augustold42@gmail.com>
2015-07-10 14:44:32 -0300

    outro readfile

--- teste.py
+++ teste.py
@@ -1,3 +1,5 @@
+import re
+
 begin = None
 end = None
 path = []
@@ -7,6 +9,20 @@ def readFile():
     global begin, end
     for line in open("./files/entrada.txt", 'r', encoding='utf-8'):
         line = line.rstrip(").\n").split("(")
+        print(line)
+        if(line[0] == "início"):
+            begin = line[1]
+        elif(line[0] == "final"):
+            end = line[1]
+        elif(line[0] == 'caminho'):
+            path.append(tuple(line[1].split(',')))
+        elif(line[0] == 'h'):
+            heuristic.append(tuple(line[1].split(',')))
+
+def readFile2():
+    global begin, end
+    for line in open("./files/entrada.txt", 'r', encoding='utf-8'):
+        line = re.split('[()]', line)
         print(line)
         if(line[0] == "início"):
             begin = line[1]
