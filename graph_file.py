d8dfff14ccc44fa574ca4d1dd0215e0e7bd9cc1a
Augusto Dantas <augustold42@gmail.com>
2015-07-19 20:12:03 -0300

    Menu muito loko

--- graph_file.py
+++ graph_file.py
@@ -13,27 +13,36 @@ class GraphFile(object):
             with open(filename, 'r', encoding='utf-8') as graph_file:
                 for line in graph_file:
                     line = line.rstrip(").\n").split("(")
-                    if(line[0] == "início"):
+                    if line[0] == "início":
                         self.begin = line[1]
-                    elif(line[0] == "final"):
+                    elif line[0] == "final":
                         self.end = line[1]
-                    elif(line[0] == 'caminho'):
+                    elif line[0] == 'caminho':
                         self.edge_list.append(line[1].split(','))
                         # converte o custo de string para inteiro
                         self.edge_list[-1][-1] = int(self.edge_list[-1][-1])
                         # adiciona o vértice na "lista" (não se repete)
                         self.vertex_set |= set(self.edge_list[-1][:2])
-                    elif(line[0] == 'h'):
+                    elif line[0] == 'h':
                         self.heuristic_list.append(line[1].split(','))
                         # converte o custo de string para inteiro
                         self.heuristic_list[-1][-1] = int(self.heuristic_list[-1][-1])
         except FileNotFoundError:
             print("Arquivo não encontrado")
 
-    def construct_graph(self):
-        graph =  {vertex:{
-            edge[1]: edge[2] for edge in self.edge_list if edge[0] == vertex
-            } for vertex in self.vertex_set}
+    def construct_graph(self, mode='directed'):
+        graph = {}
+        if mode == 'directed':
+            graph =  {vertex:{
+                edge[1]: edge[2] for edge in self.edge_list if edge[0] == vertex
+                } for vertex in self.vertex_set}
+        elif mode == 'undirected':
+            graph =  {vertex:{
+                list(set(edge[:2]) - set(vertex.splitlines()))[0]: edge[2] for edge in self.edge_list if vertex in edge
+                } for vertex in self.vertex_set}
+        else:
+            print("{} is not a valid mode.".format(mode))
+
         return graph
 
     def construct_heuristic_table(self, end=None):
@@ -42,4 +51,20 @@ class GraphFile(object):
         heuristic_table[end] = 0
         return heuristic_table
 
+    def is_heuristic_complete(self, heuristic):
+        return len(heuristic) == len(self.vertex_set)
 
+    def print_graph(self, graph):
+        print('-'*70, '\n', 'Graph'.center(70))
+        print('-'*70)
+        for vertex in self.vertex_set:
+            print("Vertex: {} ".format(vertex))
+            for edge in graph[vertex].items():
+                print('\t --> {}({})'.format(edge[0], edge[1]))
+
+    def print_heuristic(self, heuristic, end):
+        print('-'*70, '\n', 'Heuristic'.center(70))
+        print('-'*70)
+        print('Target node: {}'.format(end))
+        for vertex in self.vertex_set:
+            print('Vertex: {} = {}'.format(vertex, heuristic[vertex]))
---
+++ graph_search.py
@@ -0,0 +1,107 @@
+import sys
+from graph_file import GraphFile
+from a_star import AStar
+from dijkstra import Dijkstra
+
+graph_file = None
+graph = {}
+heuristic = {}
+begin = None
+end = None
+
+def setup(filename):
+    global graph_file, graph, begin, end, heuristic
+    graph_file = GraphFile(filename)
+    graph = graph_file.construct_graph()
+    heuristic = graph_file.construct_heuristic_table()
+    begin = graph_file.begin
+    end = graph_file.end
+
+def alter_graph_file():
+    new_file = str(input('New file path: ')).rstrip()
+    setup(new_file)
+
+def alter_end():
+    global end, heuristic
+    target = str(input('New target node: '))
+    if target in graph:
+        new_end = target
+        new_heuristic = graph_file.construct_heuristic_table(new_end)
+        if graph_file.is_heuristic_complete(new_heuristic):
+            end = new_end
+            heuristic = new_heuristic
+        else:
+            print('Error: heuristic incomplete!')
+            input('Press any key...')
+    else:
+        print('Error: nonexistent node!')
+        input('Press any key...')
+
+def alter_begin():
+    global begin
+    start = str(input('New starting node: '))
+    if start in graph:
+        begin = start
+    else:
+        print('Invalid node!')
+        input('Press any key...')
+
+def show_graph():
+    graph_file.print_graph(graph)
+    input('Press any key...')
+
+def show_heuristic():
+    graph_file.print_heuristic(heuristic, end)
+    input('Press any key...')
+
+def run_a_star():
+    AStar(graph).search_path(begin, end, heuristic)
+    input('Press any key...')
+
+def run_dijkstra():
+    Dijkstra(graph).search_path(begin, end)
+    input('Press any key...')
+
+def run_search_algorithms():
+    menu = {
+        '1': run_dijkstra,
+        '2': run_a_star,
+        '3': alter_begin,
+        '4': alter_end
+    }
+    menu_opt = ""
+    while menu_opt != '0':
+        print('-'*70, '\n', 'Search Algorithms'.center(70))
+        print('-'*70)
+        print('1 - Dijkstra')
+        print('2 - A*')
+        print('3 - Change Starting Node (current: {})'.format(begin))
+        print('4 - Change Target Node (current: {})'.format(end))
+        print('0 - Back')
+        menu_opt = input()
+        if menu_opt in menu:
+            menu[menu_opt]()
+
+def run():
+    menu = {
+        '1': run_search_algorithms,
+        '2': show_graph,
+        '3': show_heuristic,
+        '4': alter_graph_file
+    }
+    menu_opt = ""
+    while menu_opt != '0':
+        print('-'*70, '\n', 'Graph Search'.center(70))
+        print('-'*70)
+        print('1 - Run Search Algorithms')
+        print('2 - Show Graph')
+        print('3 - Show Heuristic Table')
+        print('4 - Change Graph File')
+        print('0 - Quit')
+        menu_opt = input()
+        if menu_opt in menu:
+            menu[menu_opt]()
+
+if __name__ == '__main__':
+    setup(sys.argv[1])
+    run()
